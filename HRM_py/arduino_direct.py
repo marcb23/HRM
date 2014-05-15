baf06dcabdf495fff06be72b9482c4db5c90db91
Marc Bucchieri <marc.bucchieri@tufts.edu>
2014-05-15 15:40:25 -0400

    transcribed a buncha matlab to python

--- HRM_py/arduino_direct.py
+++ HRM_py/arduino_direct.py
@@ -2,15 +2,16 @@ import time
 import numpy as np
 from collections import deque
 import serial
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+import HRMplot
 
 # credit for following two classes goes to https://gist.github.com/electronut/5641933
 # class that holds analog data for N samples
 class AnalogData:
   # constr
 	def __init__(self, maxLen):
-		self.ax = deque([0.0]*maxLen, maxLen)
-		self.ay = deque([0.0]*maxLen, maxLen)
+		self.time = deque([0.0]*maxLen, maxLen)
+		self.data = deque([0.0]*maxLen, maxLen)
 		self.maxLen = maxLen
 
   # ring buffer
@@ -23,11 +24,11 @@ class AnalogData:
 		# 	buf.append(val)
  
   # add data
-	def add(self, data):
-		print 'adding to buffer...'
+	def add(self, data_pair):
+		# print 'adding ' + str(data[1]) + ' at time ' + str(data[0])
 		assert(len(data) == 2)
-		self.addToBuf(self.ax, data[0])
-		self.addToBuf(self.ay, data[1])
+		self.addToBuf(self.time, data_pair[0])
+		self.addToBuf(self.data, data_pair[1])
     
 # plot class
 class AnalogPlot:
@@ -35,30 +36,39 @@ class AnalogPlot:
 	def __init__(self, analogData):
     # set plot to animated
 		plt.ion() 
-		self.axline, = plt.plot(analogData.ax)
+		self.axline, = plt.plot(analogData.ax, analogData.ay)
 		self.ayline, = plt.plot(analogData.ay)
 		plt.ylim([0, 1023])
  
   # update plot
 	def update(self, analogData):
-		self.axline.set_ydata(analogData.ax)
-		self.ayline.set_ydata(analogData.ay)
+		self.axline.set_ydata(analogData.ay)
+		self.axline.set_xdata(analogData.ax)
 		plt.draw()
 
 def dataLoop(ser):
 	# plot parameters
-  	analogData = AnalogData(1000)
-  	analogPlot = AnalogPlot(analogData)
+  	# analogData = AnalogData(1000)
+  	# analogPlot = AnalogPlot(analogData)
+
+  	plot = HRMplot.HRMplot()
 
 	print 'plotting...'
 	start = time.clock()
 	while True:
 		try:
+			ser.write("1")
+			# print "waiting for response.."
 			raw_in = ser.readline()
-			print raw_in
-			data = [time.clock(), float(ser.readline())]
-			analogData.add(data)
-			analogPlot.update(analogData)
+			# print raw_in
+			# try:
+			data_pair = [time.clock(), float(raw_in)]
+			# analogData.add(data)
+			# analogPlot.update(analogData)
+			# print "float works"
+			plot.add(data_pair)
+			# except:
+			# 	print "rogue string: " + raw_in
 			time.sleep(.01)
 		except KeyboardInterrupt:
 			print 'exiting'
@@ -69,8 +79,11 @@ def dataLoop(ser):
 
 
 if __name__ == '__main__':
-	ser = serial.Serial('/dev/tty.usbmodem1421', 9600)
-	print ser.readline() # Read the newest output from the Arduino	
+	ser = serial.Serial('/dev/tty.usbmodem1411', 9600)
+	connected = False
+	while not connected:
+		print ser.readline() # Read the newest output from the Arduino	
+		connected = True
 	ser.write("go")	
 	dataLoop(ser)
 	
